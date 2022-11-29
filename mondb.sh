a4295a2e483ece23acaa22e8a6e3780d86147254
bala <bala24349@gmail.com>
2022-11-29 17:10:24 +0530

    mongodb

--- mondb.sh
+++ mondb.sh
@@ -3,3 +3,10 @@ yum install -y mongodb-org
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
 systemctl start mongod
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+
+cd /tmp
+unzip mongodb.zip
+cd mongodb-main
+mongo < catalogue.js
+mongo < users.js
